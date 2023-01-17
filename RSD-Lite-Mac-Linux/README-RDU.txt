ff42cd3620482143b626250bb330220692b80c3d
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2023-01-17 09:58:15 +0100

    moving binaries and fixing the script error

---
+++ RSD-Lite-Mac-Linux/README-RDU.txt
@@ -0,0 +1,6 @@
+- I fix the error in the script
+
+- I move the binaries (rename them) because:
+  - I want to use the newest fastboot available for debian, so I simlink
+  - The script does not use adb
+  - I do not want to call the mac binaries accidentally
--- RSD-Lite-Mac-Linux/rsd-flash.sh
+++ RSD-Lite-Mac-Linux/rsd-flash.sh
@@ -1,36 +1,36 @@
 #!/bin/sh
 # Liboicl/RootJunky.com - Flash RSDLite ROM files on Linux
 # Usage: rsd-flash [XML file]
-function platform
-{       platform=`uname`
-        if [ $(uname -p) = 'powerpc' ]; then
-        echo "[-] PowerPC is not supported."
-        exit 1
-        fi
- 
-        if [ "$platform" = 'Darwin' ]; then
-        ADB="files/./adbosx"
-        FASTBOOT="files/./fastbootosx"
-        MD5SUM="md5 -r"
-        version="OS X"
-        else
-        ADB="files/./adblinux"
-        FASTBOOT="files/./fastbootlinux"
-        MD5SUM="md5sum"
-        version="Linux"
-               
-        fi
+platform () {
+    platform=`uname`
+    if [ $(uname -p) = 'powerpc' ]; then
+	echo "[-] PowerPC is not supported."
+	exit 1
+    fi
+    
+    if [ "$platform" = 'Darwin' ]; then
+	ADB="files/./adbosx"
+	FASTBOOT="files/./fastbootosx"
+	MD5SUM="md5 -r"
+	version="OS X"
+    else
+	ADB="files/./adblinux"
+	FASTBOOT="files/./fastbootlinux"
+	MD5SUM="md5sum"
+	version="Linux"
+	
+    fi
 }
 
 platform
-	echo   """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-    echo   "    ___  ____  ____  _____    ____  ___  ____ ____  __  ________  _  _    "
-    echo   "   / _ \/ _  \/ _  \/_  _/_  / / / / / \/ / //_/\ \/ / / __/ _  \/ \/ \   "
-    echo   "  / , _/ |_| / |_| / / / / // / /_/ /    /  <    \  /_/ /_/ |_| / ,  , \  "
-    echo   " /_/|_|\____/\____/ /_/  \___/\____/_/\_/_/\_/   /_/_/___/\____/_/ \/ \_\ "
-    echo   "                                                                          "
-    echo   """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-    echo "----------------------------------------------------------------------------"
+echo   """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+echo   "    ___  ____  ____  _____    ____  ___  ____ ____  __  ________  _  _    "
+echo   "   / _ \/ _  \/ _  \/_  _/_  / / / / / \/ / //_/\ \/ / / __/ _  \/ \/ \   "
+echo   "  / , _/ |_| / |_| / / / / // / /_/ /    /  <    \  /_/ /_/ |_| / ,  , \  "
+echo   " /_/|_|\____/\____/ /_/  \___/\____/_/\_/_/\_/   /_/_/___/\____/_/ \/ \_\ "
+echo   "                                                                          "
+echo   """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+echo "----------------------------------------------------------------------------"
 	echo "Welcome to RSD Lite For Mac and Linux press enter to start your flash"
 	echo "----------------------------------------------------------------------------"
     		    read \n
